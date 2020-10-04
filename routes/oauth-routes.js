5eceaee86c5ef0a328ad2cf0b5a253e5ac23a01b
zerobit <orourke_kyle@icloud.com>
2020-10-04 17:32:02 -0400

    fixing setRedirect funtion inside routes

--- routes/oauth-routes.js
+++ routes/oauth-routes.js
@@ -3,14 +3,27 @@ const passport = require("passport");
 
 console.log("HIT: oauth-routes");
 
+function setRedirect() {
+    let loginPath = "http://localhost:3001/oauth/google";
+    console.log(document.location.hostname)
+    if( document.location.hostname  === "https://fathomless-shore-38628.herokuapp.com" ) {
+        let loginPath = "https://fathomless-shore-38628.herokuapp.com/oauth/google";
+        console.log("inside if: ", loginPath)
+        return loginPath
+    } else {
+        console.log("inside else: ", loginPath)
+        return loginPath
+    }
+}
+
 // auth logout
 router
 .route("/logout")
 .get((req, res) => {
-    let redirectPath = (process.env.NODE_ENV === "production") ? "https://fathomless-shore-38628.herokuapp.com/" : "http://localhost:3000"
+    // let redirectPath = (process.env.NODE_ENV === "production") ? "https://fathomless-shore-38628.herokuapp.com/" : "http://localhost:3000"
     // let redirectPath = "http://localhost:3000"
     req.logout();
-    res.redirect(redirectPath);
+    res.redirect(setRedirect);
 });
 
 // auth with google
@@ -28,8 +41,8 @@ router
 .route('/google/redirect')
 .get(passport.authenticate("google"), (req, res) => {
 //  let redirectPath = "http://localhost:3000"
-let redirectPath = (process.env.NODE_ENV === "production") ? "https://fathomless-shore-38628.herokuapp.com" : "http://localhost:3000"
-redirectPath = `${redirectPath}/profile`;
+// let redirectPath = (process.env.NODE_ENV === "production") ? "https://fathomless-shore-38628.herokuapp.com" : "http://localhost:3000"
+redirectPath = `${setRedirect}/profile`;
 // redirectPath = `${redirectPath}/search?userId=${req.user.googleId}`
  res.redirect(redirectPath)
 })
