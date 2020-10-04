5b94ba10c20afa21325198719634de6dffa0272a
zerobit <orourke_kyle@icloud.com>
2020-10-04 13:21:30 -0400

    unignored keys.js to try and get deployed app to work

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: "541799037573-g8r0saskcnaah8pac9gmft093b10o12h.apps.googleusercontent.com",
+        clientSecret: "zamiORxo01dCmW-wIiqef1RI"
+    },
+    cookiesession: {
+        cookieKey: "randomassstringforcookieencryption"
+    },
+}
