bac4bb60d441cab4509b6ff870caa7533affc6c5
zerobit <orourke_kyle@icloud.com>
2020-10-20 15:40:47 -0400

    Updates

---
+++ precache-manifest.7bb68c73a5b6889d3f233e63bbedd9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da990e37942ed1ccbd1128d7129363c",
+    "url": "/deadstock2.0/index.html"
+  },
+  {
+    "revision": "d9752dbdfa7a7443a42f",
+    "url": "/deadstock2.0/static/css/main.a2c68817.chunk.css"
+  },
+  {
+    "revision": "241d8255b9d2af576dcf",
+    "url": "/deadstock2.0/static/js/2.94b378b7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/deadstock2.0/static/js/2.94b378b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9752dbdfa7a7443a42f",
+    "url": "/deadstock2.0/static/js/main.c13a743c.chunk.js"
+  },
+  {
+    "revision": "fb80ac7a2fae949dfccd",
+    "url": "/deadstock2.0/static/js/runtime-main.8ae5ef4a.js"
+  },
+  {
+    "revision": "eef1edb2a11d53b9a5328645d7884009",
+    "url": "/deadstock2.0/static/media/deadstock.eef1edb2.png"
+  }
+]);
