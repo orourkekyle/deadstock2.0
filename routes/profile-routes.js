b801f1cda00f67d4a7b121ed84f1fb545e1034c0
zerobit <orourke_kyle@icloud.com>
2020-10-20 14:37:26 -0400

    testing new path for sendFile

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -17,7 +17,7 @@ const authCheck = (req, res, next) => {
 router.get('/', authCheck, (req, res) => {
     console.log("inside profile route req.user: ", req.user);
     // res.render("profile", { user: req.user });
-    res.sendFile(path.join(__dirname, "../client/public/index.html", { user: req.user }));
+    res.sendFile(path.join(__dirname, "../client/src/pages/Profile.js", { user: req.user }));
 });
 
 module.exports = router;
